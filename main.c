1ddacbd5bd183b9a9a88803bc3d4c01399c0f5fc
brian-villa <brian.rj99@yahoo.com.br>
2024-01-17 00:00:01 +0000

    bug corrections

--- main.c
+++ main.c
@@ -6,43 +6,8 @@
 struct Estudantes {
     char nome[100];
     int idade;
-    char ano; // ano em que está a cursar ativo // formado ou ainda estudante
+    int ano; // ano em que está a cursar ativo // formado ou ainda estudante
 };
-
-//funcao que lista os dados em console
-void  listar() {
-    struct Estudantes studen;
-    FILE *Open;
-    Open = fopen("file.dat", "rb");
-
-    if(Open) {
-
-        //se o ficheiro existe, listar
-        fseek(Open, 0, SEEK_SET);
-        int naoSei = 0; //enquanto existem registros
-
-        printf("Nome \tIdade\tAno\n");
-
-        while(!naoSei) {
-            
-            fread(&studen, sizeof(struct Estudantes), 1, Open);
-
-            if(feof(Open)) naoSei = 1;
-
-            else {
-                printf("%s\t%d\t%s\n",
-                studen.nome,
-                studen.idade,
-                studen.ano);
-            }
-        }
-
-        fclose(Open);
-    } else {
-        printf("Ficheiro não existe...");
-    }
-};
-
 
 //funcao que adiciona dados ao arquivo
 void adicionar() {
@@ -61,11 +26,14 @@ void adicionar() {
     //Preencher informacoes
     printf("Digite o nome do estudante:");
     gets(studen.nome);
+
     printf("Digite a idade deste estudante:");
     scanf("%d", &studen.idade);
     fgetc(stdin);
-    printf("Digite a morada do estudante:");
-    gets(studen.ano);
+
+    printf("Digite o ano de ingresso do estudante:");
+    scanf("%d", &studen.ano);
+    fgetc(stdin);
 
     //fim do ficheiro
     fseek(Open, 0, SEEK_END);
@@ -75,10 +43,47 @@ void adicionar() {
     fclose(Open);
 };
 
+//funcao que lista os dados em console
+void  listar() {
+    struct Estudantes studen;
+    FILE *Open;
+    Open = fopen("file.dat", "rb");
+
+    if(Open) {
+
+        //se o ficheiro existe, listar
+
+        fseek(Open, 0, SEEK_SET);
+
+        int naoSei = 0; //enquanto existem registros
+
+        printf("Nome \tIdade\tAno\n");
+
+        while(!naoSei) {
+            
+            fread(&studen, sizeof(struct Estudantes), 1, Open);
+
+            if(feof(Open)) naoSei=1;
+
+            else {
+                printf("%s\t%d\t%d\n",
+                studen.nome,
+                studen.idade,
+                studen.ano);
+            }
+        }
+
+        fclose(Open);
+    } else {
+        printf("Ficheiro não existe...");
+    }
+};
+
 //funcao que apaga registros
 void apagar() {
 
 };
+
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
@@ -89,10 +94,11 @@ int main() {
 
     do {
         //opcoes do menu
-        printf("\n1-Adicionar Estudante\n2-Listar Estudantes\n3-Sair");
+        printf("\n1-Adicionar Estudante\n2-Listar Estudantes\n3-Sair\n");
 
         //ler acao do usuario
         scanf("%d", &menu);
+        fgetc(stdin);
 
         //validar acao do usuário
         if(menu == 1) {
