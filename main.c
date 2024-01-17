c74311b70f44a6d2af579713100255cfb6dc5f21
brian-villa <brian.rj99@yahoo.com.br>
2024-01-17 00:50:37 +0000

    year validation and bug corrections

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> //não foi ensinado na aula mas estava enfrentando um problema de erro e a solução que encontrei foi importar essa lib
 #include <locale.h>
 
 //informacao dos estudantes
@@ -7,6 +8,7 @@ struct Estudantes {
     char nome[100];
     int idade;
     int ano; // ano em que está a cursar ativo // formado ou ainda estudante
+    char status[20]; // ativo ou formado
 };
 
 //funcao que adiciona dados ao arquivo
@@ -24,16 +26,31 @@ void adicionar() {
     };
 
     //Preencher informacoes
-    printf("Digite o nome do estudante:");
+    printf("Digite o nome do estudante: ");
     gets(studen.nome);
 
-    printf("Digite a idade deste estudante:");
+    printf("Digite a idade deste estudante: ");
     scanf("%d", &studen.idade);
     fgetc(stdin);
 
-    printf("Digite o ano de ingresso do estudante:");
+    printf("Digite o ano de ingresso do estudante: ");
     scanf("%d", &studen.ano);
     fgetc(stdin);
+
+
+    //validar ano de ingresso
+    if(studen.ano < 2021) { // ano menor que 2021 licenciatura concluída
+
+        strcpy(studen.status, "Inativo/Concluido"); //copiar uma string de origem para um local de memória do obj Estudantes chamado status 
+
+    } else if (studen.ano >= 2021 && studen.ano <= 2024) { //ano de ingresso entre 2021 e 2024 curso em andamento
+        
+        strcpy(studen.status, "Ativo/Cursando");
+
+    } else {
+        printf("Ano de ingresso invalido");
+        fclose(Open);
+    }
 
     //fim do ficheiro
     fseek(Open, 0, SEEK_END);
@@ -57,7 +74,7 @@ void  listar() {
 
         int naoSei = 0; //enquanto existem registros
 
-        printf("Nome \tIdade\tAno\n");
+        printf("Nome \tIdade\tAno\tStatus de Matricula\n");
 
         while(!naoSei) {
             
@@ -66,16 +83,17 @@ void  listar() {
             if(feof(Open)) naoSei=1;
 
             else {
-                printf("%s\t%d\t%d\n",
+                printf("%s\t%d\t%d\t%s\n",
                 studen.nome,
                 studen.idade,
-                studen.ano);
+                studen.ano,
+                studen.status);
             }
         }
 
         fclose(Open);
     } else {
-        printf("Ficheiro não existe...");
+        printf("Ficheiro não existe...\n");
     }
 };
 
